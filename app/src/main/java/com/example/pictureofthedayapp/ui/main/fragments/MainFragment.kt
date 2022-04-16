cfd98a55bdba8019c86433d9ee6998a4655dd3bf
stepurko <stepurko@vbankcenter.ru>
2022-04-16 14:07:50 +0300

    Added main_fragment.xml layout with BottomSheetDialog

--- app/src/main/java/com/example/pictureofthedayapp/ui/main/fragments/MainFragment.kt
+++ app/src/main/java/com/example/pictureofthedayapp/ui/main/fragments/MainFragment.kt
@@ -1,16 +1,24 @@
 package com.example.pictureofthedayapp.ui.main.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import coil.annotation.ExperimentalCoilApi
 import coil.load
 import com.example.pictureofthedayapp.R
 import com.example.pictureofthedayapp.ui.main.viewmodels.LoadState
 import com.example.pictureofthedayapp.ui.main.viewmodels.MainViewModel
+import com.example.pictureofthedayapp.utils.showTextToast
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class MainFragment : Fragment() {
 
@@ -19,28 +27,83 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    private lateinit var progress: CircularProgressIndicator
+    private lateinit var ivPicture: AppCompatImageView
+    private lateinit var bottomSheet: ConstraintLayout
+    private lateinit var bottomSheetTvDescriptionHeader: AppCompatTextView
+    private lateinit var bottomSheetTvDescription: AppCompatTextView
+    private lateinit var btnReload: AppCompatButton
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    @OptIn(ExperimentalCoilApi::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initObservers(view)
+        initViews(view)
+        initObservers()
         viewModel.getData()
     }
 
-    private fun initObservers(view: View) {
+    private fun initViews(view: View) {
+        progress = view.findViewById(R.id.progress_circular)
+        ivPicture = view.findViewById(R.id.iv_picture)
+        bottomSheet = view.findViewById(R.id.bottom_sheet_container)
+        bottomSheetTvDescriptionHeader =
+            bottomSheet.findViewById(R.id.bottom_sheet_description_header)
+        bottomSheetTvDescription = bottomSheet.findViewById(R.id.bottom_sheet_description)
+        btnReload = view.findViewById(R.id.btn_reload)
+
+        btnReload.setOnClickListener {
+            viewModel.getData()
+        }
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.skipCollapsed = true
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    @ExperimentalCoilApi
+    private fun initObservers() {
         viewModel.liveData.observe(viewLifecycleOwner) {
             when (it) {
-                is LoadState.Error -> {}
-                is LoadState.Loading -> {}
+                is LoadState.Error -> {
+                    progress.isVisible = false
+                    btnReload.isVisible = true
+                    ivPicture.isVisible = false
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                    it.error.message?.let { errorMessage -> showTextToast(errorMessage) }
+                }
+                is LoadState.Loading -> {
+                    progress.isVisible = true
+                    btnReload.isVisible = false
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                }
                 is LoadState.Success -> {
+                    progress.isVisible = false
+                    btnReload.isVisible = false
                     with(it.data) {
-                        val ivPicture = view.findViewById<AppCompatImageView>(R.id.iv_picture)
-                        ivPicture.load(this.url)
+                        ivPicture.load(this.url) {
+                            listener(
+                                onSuccess = { _, _ ->
+                                    ivPicture.isVisible = true
+                                },
+                                onError = { _, _ ->
+                                    ivPicture.isVisible = false
+                                }
+                            )
+                        }
+
+                        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+                        bottomSheetTvDescriptionHeader.text = this.title
+                        bottomSheetTvDescription.text = this.explanation
                     }
                 }
             }
